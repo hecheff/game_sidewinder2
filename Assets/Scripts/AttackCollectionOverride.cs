9568c8658294e422813f37c38c9f98222b9051ef
hecheff <44626838+hecheff@users.noreply.github.com>
2019-12-03 23:16:39 +0900

    Update 2019/12/03
    Improved override to automatically update values. Replaces manual flag input approach.

--- Assets/Scripts/AttackCollectionOverride.cs
+++ Assets/Scripts/AttackCollectionOverride.cs
@@ -4,40 +4,60 @@ using UnityEngine;
 
 // Variable overrides for Attacks
 public class AttackCollectionOverride : MonoBehaviour {
-
-    [Header("Update Flag")]
-    [Tooltip("Enable once during runtime to override existing settings")]
-    public bool updateFlag = false;
-
     [Header("Basic Shot Properties")]
     public GameObject           basicShot_playerShotPool;                                   // Shot Pool
     public List<ShotProperties> basicShot_shotProperties    = new List<ShotProperties>();   // List of all objects in pool
-    public float                basicShot_shotSpeed         = 0.0f;                         // Shot velocity
-    public Axis                 basicShot_rotationAxis      = Axis.Z;                       // Rotation Axis 
-    public float                basicShot_rotationAngle     = 0.0f;                         // Rotation Angle
-    
+    public float    basicShot_shotSpeed         = 0.0f;         // Shot velocity
+    public Axis     basicShot_rotationAxis      = Axis.Z;       // Rotation Axis 
+    public float    basicShot_rotationAngle     = 0.0f;         // Rotation Angle
+    private float    ref_basicShot_shotSpeed;
+    private Axis     ref_basicShot_rotationAxis;
+    private float    ref_basicShot_rotationAngle;
+
+    [Header("Laser Properties")]
+    public GameObject laser;
+
     void Awake() {
         // Populate Basic Shot attributes
         foreach (Transform child in basicShot_playerShotPool.transform) {
             var currShotProperties = child.gameObject.GetComponent<ShotProperties>();
-
+            
+            // Set basic properties
             basicShot_shotProperties.Add(currShotProperties);
             basicShot_shotSpeed     = currShotProperties.shotSpeed;
             basicShot_rotationAxis  = currShotProperties.rotationAxis;
             basicShot_rotationAngle = currShotProperties.rotationAngle;
         }
+        // Set reference values
+        ref_basicShot_shotSpeed     = basicShot_shotSpeed;
+        ref_basicShot_rotationAxis  = basicShot_rotationAxis;
+        ref_basicShot_rotationAngle = basicShot_rotationAngle;
+
+        // Populate Laser attributes
+
     }
 
     void Update() {
-        if(updateFlag) {
+        // Update basic shot attributes if change detected
+        if(DetectChange_BasicShot()) {
             for(int i = 0; i < basicShot_shotProperties.Count; i++) {
                 basicShot_shotProperties[i].shotSpeed       = basicShot_shotSpeed;
                 basicShot_shotProperties[i].rotationAxis    = basicShot_rotationAxis;
                 basicShot_shotProperties[i].rotationAngle   = basicShot_rotationAngle;
                 basicShot_shotProperties[i].updateOverride  = true;
             }
-
-            updateFlag = false;
         }
+    }
+
+    // Detect changes in Basic Shot values
+    bool DetectChange_BasicShot() {
+        if((ref_basicShot_shotSpeed != basicShot_shotSpeed) || 
+            (ref_basicShot_rotationAxis != basicShot_rotationAxis) || 
+            (ref_basicShot_rotationAngle != basicShot_rotationAngle)) {
+            ref_basicShot_shotSpeed     = basicShot_shotSpeed;
+            ref_basicShot_rotationAxis  = basicShot_rotationAxis;
+            ref_basicShot_rotationAngle = basicShot_rotationAngle;
+            return true;
+        } return false;
     }
 }
